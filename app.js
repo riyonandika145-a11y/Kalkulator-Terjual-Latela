f3639b7ef7325ebeec335c3feaf22e358cb9d5c4
riyonandika145-a11y <riyonandika145@gmail.com>
2026-06-19 11:38:41 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 // CLOUD DATABASE CONFIGURATION (GOOGLE SHEETS)
 // =========================================================================
 // Link Web App Google Apps Script terpasang rapi di sini:
-const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzc-lYZGC5958wLSHfhlnTr1u0GeKP-6vEYRgIoSkrn98j1oVMhSTKJE9y4hWJsPPY/exec";
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbx4fsOMfhCGcxRa3GXkjC0Eg8AJOoY8EGnTMBoxVQKG7nt2LvzZufMe_vqq8hUii3E/exec";
 
 // --- DOM SELEKTORS ---
 const menuItems = document.querySelectorAll('.menu-item');
